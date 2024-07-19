0dd081dda5c7cbc4fc4de5d7a83dc54352dd32aa
lucaEnergent <g.luzzi@energent.it>
2024-07-19 17:52:07 +0200

    aggiunta gestione errori

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.entity.User;
+import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.repository.UserRepository;
 
 @Controller
@@ -22,15 +24,23 @@ public class UserController {
 	}
 
 	@PostMapping("/form")
-    public String inserisciDati(User user) {
-        repo.save(user);
-        return "redirect:/user";
-    }
-	
+	public String inserisciDati(User user) {
+		repo.save(user);
+		return "redirect:/user";
+	}
+
 	@GetMapping("/user")
-    public String listaUsers(Model model) {
-        model.addAttribute("users", repo.findAll());
-        return "user";
-    }
-	
+	public String listaUsers(Model model) {
+		model.addAttribute("users", repo.findAll());
+		return "user";
+	}
+
+	@GetMapping("/user/{id}")
+	public String leggiUserById(@PathVariable Long id, Model model) {
+		User user = repo.findById(id).orElseThrow(() -> new UserNotFoundException("Utente non trovato con id: " + id));
+		model.addAttribute("user", user);
+
+		return "user-details";
+	}
+
 }
